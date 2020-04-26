eb3cf2fb1c34bef589a95687762447ebc7624f8f
Krungleviciute <d.krungleviciute@gmail.com>
2020-04-26 12:37:14 +0300

    Updates

---
+++ precache-manifest.97fd8e97e6c7b41c375ad74a86a6ed83.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5417a0aa75e334085f3e61b9ddd390b",
+    "url": "/web-template/index.html"
+  },
+  {
+    "revision": "da75d82cea7724ec798b",
+    "url": "/web-template/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a6b1f818e5067b2fce50",
+    "url": "/web-template/static/css/main.9b736398.chunk.css"
+  },
+  {
+    "revision": "da75d82cea7724ec798b",
+    "url": "/web-template/static/js/2.47d4a48f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-template/static/js/2.47d4a48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b1f818e5067b2fce50",
+    "url": "/web-template/static/js/main.1504d860.chunk.js"
+  },
+  {
+    "revision": "c4f5b391874030c08908",
+    "url": "/web-template/static/js/runtime-main.f24cb1d0.js"
+  },
+  {
+    "revision": "ebff4a19cb3cd1c95eda727f20a7b039",
+    "url": "/web-template/static/media/Elephant.ebff4a19.png"
+  },
+  {
+    "revision": "e5b2f99c96617b126977605ba5fe848c",
+    "url": "/web-template/static/media/ForestPath.e5b2f99c.png"
+  },
+  {
+    "revision": "b42582ce99a72934da09b98db1f6d2a7",
+    "url": "/web-template/static/media/SanFrancisco.b42582ce.png"
+  },
+  {
+    "revision": "767d20458794b6fe5674961506832239",
+    "url": "/web-template/static/media/hero-image.767d2045.png"
+  },
+  {
+    "revision": "718db5f8db05e7c1406f02590ba29fa9",
+    "url": "/web-template/static/media/parrot.718db5f8.png"
+  },
+  {
+    "revision": "5f4dde5b21c9a82d89ff4ec25b7a161c",
+    "url": "/web-template/static/media/road.5f4dde5b.png"
+  },
+  {
+    "revision": "98d3d3dbae7af09bc2e6975c00d1851a",
+    "url": "/web-template/static/media/trending-image-1.98d3d3db.png"
+  },
+  {
+    "revision": "b7d073a99d6fede93d0bce70ca78bbee",
+    "url": "/web-template/static/media/trending-image-2.b7d073a9.png"
+  },
+  {
+    "revision": "4f403975d98ee79c5a8d3c6e3b0ede7c",
+    "url": "/web-template/static/media/trending-image-3.4f403975.png"
+  }
+]);
